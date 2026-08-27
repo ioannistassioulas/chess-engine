893d75cab6938731813d4073dd308ceca56765d9
ioannistassioulas <i.a.tassioulas@gmail.com>
2026-08-27 23:23:31 +0300

    First Commit

---
+++ src/chess-engine.py
@@ -0,0 +1,13 @@
+"""
+THis is my variation of 
+"""
+
+import chess
+
+class BassHunter():
+    """
+    Homemade chess engine
+    """
+
+    def __init_(self, state):
+        self.state = state
---
+++ src/chess-movie-player.py
@@ -0,0 +1,5 @@
+import chess
+import chess.svg
+
+def chess_movie():
+    """Take file of chess notation and produce gif showing game."""
